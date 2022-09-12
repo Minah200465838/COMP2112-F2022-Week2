77c4c52c3b0077dfae9d9008a403c6015bac5b12
Minah Cho <19972001@gmail.com>
2022-09-12 16:11:39 -0400

    initial commit

---
+++ Scripts/app.js
@@ -0,0 +1,11 @@
+//IIFE -- Immediately Invoked function Expression
+// AKA - Self Executing Function
+(function()
+{
+function Start()
+{
+    console.log("App Started!");
+}
+    window.addEventListener()
+}
+)
